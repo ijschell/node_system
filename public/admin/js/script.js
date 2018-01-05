1054f41da17ce2a2df7047ff9d8b9d70b78bd52f
Jonathan <jonathans@takeoffmedia.com>
2018-01-05 00:21:57 -0300

    secciones style casi lista

--- public/admin/js/script.js
+++ public/admin/js/script.js
@@ -1,3 +1,11 @@
+$(document).ready(function(){
+
+    activeTinymce();
+
+    convertStringToHtml();
+
+})
+
 $(document).on('click', '#contentSections .actions .edit', function(e){
 
     e.preventDefault();
@@ -23,3 +31,28 @@ $(document).on('blur', '#perfilSection input[name="pass"]', function(){
 $.validate({
     modules : 'security'
 })
+
+
+// Active Tinymce
+function activeTinymce(){
+
+    ClassicEditor
+        .create( document.querySelector( '.editor' ))
+        .catch( error => {
+            console.error( error );
+        } );
+
+}
+
+
+function convertStringToHtml(){
+
+    // var markup = '<div><p>text here</p></div>';
+    $('#contentSections .section').each(function(k, v){
+        var markup = $(v).find('.view_content .content_description').text();
+        $(v).find('.view_content .content_description').html(markup);
+    })
+    // var parser = new DOMParser()
+    // var el = parser.parseFromString(markup, "text/xml");
+
+}
